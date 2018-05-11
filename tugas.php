c031e6aced42c558c6fcccc37f8aa96e12563fdb
muhfajarprawira <muh.fajar.prawira@student.untan.ac.id>
2018-05-12 02:14:25 +0700

    autoload

---
+++ tugas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ini judul percobaan</title>
+</head>
+<body>
+		<h1>ini judul</h1>
+
+		<p>sdafmkdfmgkmdkgmkermkgmekdmgkmfdkgmkermrkmkemgkemkerg
+		sadadasd
+	dasadasdas
+dasdsadasdasdas</p>
+
+<?php 
+echo "<h2>ini tulisan ".$angka."</h2>";
+?>
+</body>
+
+</html>
